ee227211ed62d703607bf29390f80513f793e683
VenkataAshrith <shekallavenkataashrith@gmail.com>
2024-08-31 09:40:41 +0530

    routed the pages

--- facultyfeedbacksystem/src/Components/AdminDashboard.jsx
+++ facultyfeedbacksystem/src/Components/AdminDashboard.jsx
@@ -3,6 +3,7 @@ const AdminDashboard = () =>{
     return(
         <div>
             <h1>Admin Dashboard</h1>
+            <p>welcome Admin to the portal of feedback system.</p>
         </div>
     );
 };
